20251aadf82c076f436b7ff37bc3d421563feaec
Kevin Wong <kevin.wong954@gmail.com>
2023-08-07 13:40:23 -0400

    work through LisaHJung's Mini Beginner's Crash Course to ES & Kibana season 1 and season 2

---
+++ season2/earthquake_app/server/server.js
@@ -0,0 +1,70 @@
+const { Client } = require("@elastic/elasticsearch");
+const client = require("./elasticsearch/client");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const data = require("./data_management/retrieve_and_ingest_data");
+
+app.use("/ingest_data", data);
+
+app.use(cors());
+
+app.get("/results", (req, res) => {
+  const passedType = req.query.type;
+  const passedMag = req.query.mag;
+  const passedLocation = req.query.location;
+  const passedDateRange = req.query.dateRange;
+  const passedSortOption = req.query.sortOption;
+
+  async function sendESRequest() {
+    const body = await client.search({
+      index: "earthquakes",
+      body: {
+        sort: [
+          {
+            mag: {
+              order: passedSortOption,
+            },
+          },
+        ],
+        size: 300,
+        query: {
+          bool: {
+            filter: [
+              {
+                term: { type: passedType },
+              },
+              {
+                range: {
+                  mag: {
+                    gte: passedMag,
+                  },
+                },
+              },
+              {
+                match: { place: passedLocation },
+              },
+              // for those who use prettier, make sure there is no whitespace.
+              {
+                range: {
+                  "@timestamp": {
+                    gte: `now-${passedDateRange}d/d`,
+                    lt: "now/d",
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+    res.json(body.hits.hits);
+  }
+  sendESRequest();
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.group(`Server started on ${PORT}`));
